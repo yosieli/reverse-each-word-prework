817049b273f3bf643d43e715efa6a8fe0f0df07d
feven <feventsegay6@gmail.com>
2019-04-20 16:33:31 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+def reverse_each_word(string)
+  array = string.split(" ")
+  new_array = []
+array.each do|string|
+    new_array << string.reverse
+  end
+  new_array.join(" ")
+end
+def reverse_each_word(string)
+  array = string.split(" ")
+  test_array = []
+  array.collect do|string|
+    test_array << string.reverse 
+  end
+  test_array.join(" ")
+end
+
+
